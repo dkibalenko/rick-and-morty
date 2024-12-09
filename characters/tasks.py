787285590c22c594d71f3640fef3f18cb531ff86
Denys Kibalenko <dennieking87@gmail.com>
2024-12-09 11:38:18 +0100

    Feat: add PostgreSQL support & setup .env file

--- characters/tasks.py
+++ characters/tasks.py
@@ -6,7 +6,3 @@ from celery import shared_task
 @shared_task
 def run_sync_with_api() -> None:
     sync_characters_with_api()
-
-# celery -A rick_and_morty_api worker -l INFO --pool=solo
-# (or celery -A rick_and_morty_api worker -l INFO --pool=gevent)
-# celery -A rick_and_morty_api beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
